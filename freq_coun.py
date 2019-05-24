726b695bc5a17daab047ab776c0f9149be6b710d
himanshuchelani <himanshuchelani95@gmail.com>
2019-05-24 20:15:52 +0530

    frequency counet

---
+++ freq_coun.py
@@ -0,0 +1,10 @@
+def freq(str1):
+    dict={}
+    for n in str1:
+        keys=dict.keys()
+        if n in keys:
+            dict[n]+=1
+        else:
+            dict[n]=1
+    return dict
+print(freq('google.com'))
